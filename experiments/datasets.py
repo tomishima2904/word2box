29a9151dc965af9f5c12ce040c2ecfb8e5f0363b
tomishima <pumoco2904@icloud.com>
2022-12-13 13:10:52 +0900

    全ての語彙と評価を行うためのDatasetを用意

---
+++ experiments/datasets.py
@@ -0,0 +1,38 @@
+import torch
+from torch import LongTensor, BoolTensor, Tensor
+from torch.utils.data import Dataset
+from typing import List, Tuple, Dict, Any, Union
+
+from pytorch_utils import TensorDataLoader
+
+from language_modeling_with_boxes.box.box_wrapper import BoxTensor
+from language_modeling_with_boxes.models import Word2Box, Word2Vec, Word2VecPooled, Word2BoxConjunction, Word2Gauss
+
+
+# Dataset to evaluate similarity from all vocabulary
+# Vocabulary にある全ての語彙に対して類似度を計算するための評価用データセット
+class TrainedAllVocabDataset(Dataset):
+    def __init__(
+        self,
+        vocab_stoi: dict,
+        model: Union[Word2Box, Word2Vec, Word2VecPooled, Word2BoxConjunction, Word2Gauss]
+    ):
+        super().__init__()
+        self.vocab_stoi = vocab_stoi
+        self.model = model
+
+
+    def __getitem__(self) -> LongTensor:
+        with torch.no_grad():
+            vocab_ids = LongTensor([v_id for v_id in self.vocab_stoi.values()])
+            all_boxes: BoxTensor = self.model.embeddings_word(vocab_ids)
+
+            # Convert TensorBox to LongTensor size of which is [len(vocab_stoi), 2, embedding_dim]
+            all_zZ = torch.stack([all_boxes.z, all_boxes.Z], dim=-2)
+
+            return all_zZ
+
+
+    def __len__(self) -> int:
+        return len(self.vocab_stoi)
+
