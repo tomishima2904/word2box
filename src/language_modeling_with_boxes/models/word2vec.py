1ab88233587fbc134ac555da0cec8d8bbbaac1e6
tomishima <pumoco2904@icloud.com>
2023-02-04 20:15:59 +0900

    Changed arg of Word2Vec

--- src/language_modeling_with_boxes/models/word2vec.py
+++ src/language_modeling_with_boxes/models/word2vec.py
@@ -18,13 +18,13 @@ device = 0 if use_cuda else -1
 
 
 class Word2Vec(BaseModule):
-    def __init__(self, TEXT=None, embedding_dim=50, batch_size=10, n_gram=4, **kwargs):
+    def __init__(self, vocab_size=None, embedding_dim=50, batch_size=10, n_gram=4, **kwargs):
         super(Word2Vec, self).__init__()
 
         # Model
         self.batch_size = batch_size
         self.n_gram = n_gram
-        self.vocab_size = len(TEXT.itos)
+        self.vocab_size = vocab_size
         self.embedding_dim = embedding_dim
 
         # Create embeddings
