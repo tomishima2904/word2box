949b2f8a52b2fa18a1692271b7e8cb27ba815661
tomishima <pumoco2904@icloud.com>
2023-03-14 15:50:29 +0900

    Added some funcs to class `VocabLibsWithFreq`

--- src/experiments/modules/vocab_libs.py
+++ src/experiments/modules/vocab_libs.py
@@ -2,6 +2,8 @@ import torch
 from torch import Tensor, LongTensor
 from typing import Union, List, Dict
 import json
+import numpy as np
+import matplotlib.pyplot as plt
 
 
 class VocabLibs(object):
@@ -62,3 +64,55 @@ class VocabLibsWithFreq(VocabLibs):
     def __init__(self, vocab_stoi_path: str, vocab_freq_path: str = None) -> None:
         super().__init__(vocab_stoi_path)
         self.vocab_freq: Dict = json.load(open(vocab_freq_path, "r"))
+
+
+    # Sort freq
+    def _sort_freq(self) -> List:
+        freq_list = [v for v in self.vocab_freq.values()]
+        freq_list.sort(reverse=True)
+        return freq_list
+
+
+    # Dump sorted vocab freqs (without tokens)
+    def dump_sorted_freq(self, output_path) -> None:
+        freq_list = self._sort_freq()
+        with open(output_path, "w") as f:
+            for freq in freq_list:
+                print(freq, file=f)
+
+
+    # Plot sorted vocab freqs
+    def plot_sorted_freq(self, input_path, output_dir, x_limit=None) -> None:
+        with open(input_path, "r") as f:
+            sorted_freqs = np.array([int(line) for line in f])
+
+            if x_limit != None:
+                sorted_freqs = sorted_freqs[:x_limit]
+                save_path = f"{output_dir}/sorted_freq_{x_limit}.png"
+            else:
+                save_path = f"{output_dir}/sorted_freq_all.png"
+
+            x = np.arange(len(sorted_freqs))
+            plt.yscale('log')
+            plt.plot(x, freqs)
+            plt.title('Sorted frequency')
+            plt.xlabel('Vocab')
+            plt.ylabel('Frequency')
+            plt.savefig(save_path, format="png")
+
+
+    # Count num of vocab freq of which is less than x
+    def count_less_freq(self, x) -> int:
+        count = 0
+        for v in self.vocab_freq.values():
+            if v < x:
+                count += 1
+        return count
+
+
+    # Count num of all tokens
+    def count_all_tokens(self) -> int:
+        count = 0
+        for v in self.vocab_freq.values():
+            count += v
+        return count
