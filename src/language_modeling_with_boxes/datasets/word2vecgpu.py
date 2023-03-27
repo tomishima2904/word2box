f172814ae91e2fb79fe52f2d6e1d2ed80cbffb91
tomishima <pumoco2904@icloud.com>
2023-03-27 15:41:46 +0900

    CHANGED DATALOADER

--- src/language_modeling_with_boxes/datasets/word2vecgpu.py
+++ src/language_modeling_with_boxes/datasets/word2vecgpu.py
@@ -2,7 +2,7 @@ import torch
 from torch import LongTensor, BoolTensor, Tensor
 from torch.utils.data import Dataset
 from typing import List, Tuple, Dict, Any, Union
-
+from torch.utils.data import DataLoader
 from pytorch_utils import TensorDataLoader
 
 import attr
@@ -184,7 +184,8 @@ class LazyDatasetLoader:
                 eos_mask=self.eos_mask,
                 ignore_unk=self.ignore_unk,
             ).to(self.device)
-            train_iter = TensorDataLoader(train_dataset, self.batch_size, shuffle=True)
+            #  train_iter = TensorDataLoader(train_dataset, self.batch_size, shuffle=True)
+            train_iter = DataLoader(train_dataset, self.batch_size, shuffle=True)
             yield from train_iter
             del train_dataset
             del train_iter
